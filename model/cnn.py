2776480b087243c06d01795b76fbab7d407c0405
Frank He <frankheshibi@gmail.com>
2017-11-28 17:47:09 -0800

    batch norm finished

--- model/cnn.py
+++ model/cnn.py
@@ -14,7 +14,7 @@ sess = tf.InteractiveSession()
 
 label_size = 19
 learning_rate = 0.001
-device = '/cpu:0'
+device = '/gpu:0'
 
 with tf.device(device):
     X_batch = tf.placeholder(shape=(None, 26, 31, 23, 1), dtype=tf.float32, name='X_batch')
@@ -22,66 +22,75 @@ with tf.device(device):
     global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
     optimizer = tf.train.AdamOptimizer(learning_rate)
 
-    # with tf.variable_scope('bn1'):
-    #     X_batch_norm = tf.layers.batch_normalization(X_batch, training=training_flag)
 
-    with tf.variable_scope('conv1'):
-        channels = 1; filters = 4; size = 3; stride = 1;
-        X_batch_conv1 = ops.conv3d(X_batch, channels, filters, stride, size, size, size, padding='SAME')
+def build_network(X_batch, training_flag):
+        with tf.variable_scope('bn1'):
+            X_batch_norm = tf.layers.batch_normalization(X_batch, training=training_flag)
 
-    with tf.variable_scope('conv1_2'):
-        channels = 4; filters = 8; size = 3; stride = 1;
-        X_batch_conv1_2 = ops.conv3d(X_batch_conv1, channels, filters, stride, size, size, size, padding='SAME')
+        with tf.variable_scope('conv1'):
+            channels = 1; filters = 4; size = 3; stride = 1;
+            X_batch_conv1 = ops.conv3d(X_batch_norm, channels, filters, stride, size, size, size, padding='SAME')
 
-    with tf.variable_scope('pool1'):
-        size = 2; stride = 2;
-        X_batch_pool1 = tf.nn.max_pool3d(X_batch_conv1_2, ksize=[1, size, size, size, 1],
-                                         strides=[1, stride, stride, stride, 1], padding="SAME")
+        with tf.variable_scope('conv1_2'):
+            channels = 4; filters = 8; size = 3; stride = 1;
+            X_batch_conv1_2 = ops.conv3d(X_batch_conv1, channels, filters, stride, size, size, size, padding='SAME')
 
-    # with tf.variable_scope('bn2'):
-    #     X_batch_norm2 = tf.layers.batch_normalization(X_batch_pool1, training=training_flag)
+        with tf.variable_scope('pool1'):
+            size = 2; stride = 2;
+            X_batch_pool1 = tf.nn.max_pool3d(X_batch_conv1_2, ksize=[1, size, size, size, 1],
+                                             strides=[1, stride, stride, stride, 1], padding="SAME")
 
-    with tf.variable_scope('conv2'):
-        channels = 8; filters = 16; size = 3; stride = 1;
-        X_batch_conv2 = ops.conv3d(X_batch_pool1, channels, filters, stride, size, size, size, padding='SAME')
+        with tf.variable_scope('bn2'):
+            X_batch_norm2 = tf.layers.batch_normalization(X_batch_pool1, training=training_flag)
 
-    with tf.variable_scope('conv2_2'):
-        channels = 16; filters = 32; size = 3; stride = 1;
-        X_batch_conv2_2 = ops.conv3d(X_batch_conv2, channels, filters, stride, size, size, size, padding='SAME')
+        with tf.variable_scope('conv2'):
+            channels = 8; filters = 16; size = 3; stride = 1;
+            X_batch_conv2 = ops.conv3d(X_batch_norm2, channels, filters, stride, size, size, size, padding='SAME')
 
-    with tf.variable_scope('pool2'):
-        size = 2; stride = 2;
-        X_batch_pool2 = tf.nn.max_pool3d(X_batch_conv2_2, ksize=[1, size, size, size, 1],
-                                         strides=[1, stride, stride, stride, 1], padding="SAME")
+        with tf.variable_scope('conv2_2'):
+            channels = 16; filters = 32; size = 3; stride = 1;
+            X_batch_conv2_2 = ops.conv3d(X_batch_conv2, channels, filters, stride, size, size, size, padding='SAME')
 
-    conv_out_shape = X_batch_pool2.get_shape().as_list()
-    conv_out_shape[0] = 1
-    dim = reduce(lambda x, y: x * y, conv_out_shape)
+        with tf.variable_scope('pool2'):
+            size = 2; stride = 2;
+            X_batch_pool2 = tf.nn.max_pool3d(X_batch_conv2_2, ksize=[1, size, size, size, 1],
+                                             strides=[1, stride, stride, stride, 1], padding="SAME")
 
-    X_batch_reshape = tf.reshape(X_batch_pool2, [-1, dim])
+        conv_out_shape = X_batch_pool2.get_shape().as_list()
+        conv_out_shape[0] = 1
+        dim = reduce(lambda x, y: x * y, conv_out_shape)
 
-    with tf.variable_scope('linear1'):
-        out_dim = 512
-        X_batch_linear1 = ops.linear_layer(X_batch_reshape, dim, out_dim)
+        X_batch_reshape = tf.reshape(X_batch_pool2, [-1, dim])
 
-    with tf.variable_scope('linear2'):
-        out_dim = label_size; dim = 512;
-        X_batch_linear2 = ops.linear_layer(X_batch_linear1, dim, out_dim)
+        with tf.variable_scope('linear1'):
+            out_dim = 512
+            X_batch_linear1 = ops.linear_layer(X_batch_reshape, dim, out_dim)
 
-    logits = X_batch_linear2
+        with tf.variable_scope('linear2'):
+            out_dim = label_size; dim = 512;
+            X_batch_linear2 = ops.linear_layer(X_batch_linear1, dim, out_dim)
 
-    Y_prediction = tf.nn.sigmoid(logits)
+        logits = X_batch_linear2
 
-    cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=Y_batch, logits=logits)
+        return logits
 
-    loss = tf.reduce_mean(cross_entropy)
 
-    grad_var_list = optimizer.compute_gradients(loss)
-    apply_gradients_op = optimizer.apply_gradients(grad_var_list, global_step)
+with tf.device(device):
+    with tf.variable_scope('network') as current_scope:
+        logits = build_network(X_batch, training_flag=True)
+        cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=Y_batch, logits=logits)
+        loss = tf.reduce_mean(cross_entropy)
+        grad_var_list = optimizer.compute_gradients(loss)
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            apply_gradients_op = optimizer.apply_gradients(grad_var_list, global_step)  # this is training op
 
+        current_scope.reuse_variables()
+        logits2 = build_network(X_batch, training_flag=False)
+        Y_prediction = tf.nn.sigmoid(logits2)  # this is evaluation op
 
 epochs = 30
-batch_size = 8
+batch_size = 16
 
 sess.run(tf.global_variables_initializer())
 
