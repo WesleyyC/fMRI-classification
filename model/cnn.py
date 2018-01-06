467677e09dafa4aac497ec0d18dac49be7cfae0a
wesleyyc <wesley.chin0919@gmail.com>
2018-01-06 12:21:55 +0800

    final update

--- model/cnn.py
+++ model/cnn.py
@@ -1,105 +1,238 @@
 import numpy as np
 import tensorflow as tf
-import tensorflow.contrib as tfc
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score
 from sklearn.utils import shuffle
 
 import ops
+import utils
 
+# Mode
+
+infer_only = False
+
+# Load data
 train_X = np.load('../data/train_X.npy')
-train_binary_Y = np.load('../data/train_binary_Y.npy')
-valid_test_X = np.load('../data/valid_test_X.npy')
+train_Y = np.load('../data/train_binary_Y.npy')
+
+# Process Data
+
+train_X = utils.normalized_data(train_X)
+
+train_X = train_X[:, :, :, :, np.newaxis]
+
+# Shuffle and Divide Two Train/Test
+train_X, train_Y = shuffle(train_X, train_Y)
+sample_number = len(train_X)
+
+test_fraction = 0.1
+test_range = int(test_fraction * sample_number)
+test_X = train_X[:test_range]
+train_X = train_X[test_range:]
+test_Y = train_Y[:test_range]
+train_Y = train_Y[test_range:]
+
+# train_X, train_Y = utils.resample_data(train_X, train_Y)
+
+# Model Parameter
+
+label_size = 19
+
+regularizer_scale = 0.0
+
+starting_learning_rate = 0.001
+decay_step = 50
+decay_rate = 0.98
+
+# Build NN Graph
 
 tf.reset_default_graph()
 sess = tf.InteractiveSession()
 
-label_size = 19
-learning_rate = 0.001
-device = '/gpu:0'
+X_batch = tf.placeholder(shape=(None, 26, 31, 23, 1), dtype=tf.float32, name='X_batch')
+Y_batch = tf.placeholder(shape=(None, 19), dtype=tf.float32, name='Y_batch')
+training_flag = tf.placeholder(dtype=tf.bool, name='training_flag')
+keep_prob = tf.placeholder(tf.float32)
 
-with tf.device(device):
-    X_batch = tf.placeholder(shape=(None, 26, 31, 23, 1), dtype=tf.float32, name='X_batch')
-    Y_batch = tf.placeholder(shape=(None, 19), dtype=tf.float32, name='Y_batch')
-    global_step = tf.get_variable('global_step', [], initializer=tf.constant_initializer(0), trainable=False)
-    optimizer = tf.train.AdamOptimizer(learning_rate)
+regularizer = tf.contrib.layers.l2_regularizer(scale=regularizer_scale)
 
+noise_layer_1 = ops.gaussian_noise_layer(X_batch, .5, training_flag)
 
-def build_network(X_batch, training_flag):
-        with tf.variable_scope('bn1'):
-            X_batch_norm = tf.layers.batch_normalization(X_batch, training=training_flag)
+kernel_size = 32
+stride = 1
+filter_depth = 4
+filter_height = 4
+filter_width = 4
+conv_layer_1 = ops.conv3d_block(X_batch, training_flag, kernel_size, stride, filter_depth, filter_height,
+                                filter_width, regularizer, 1, padding="SAME")
 
-        with tf.variable_scope('conv1'):
-            channels = 1; filters = 4; size = 3; stride = 1;
-            X_batch_conv1 = ops.conv3d(X_batch_norm, channels, filters, stride, size, size, size, padding='SAME')
+pool_size = 2
+pool_stride = pool_size
+pool_layer_1 = tf.nn.max_pool3d(conv_layer_1, [1, pool_size, pool_size, pool_size, 1],
+                                [1, pool_stride, pool_stride, pool_stride, 1], padding="SAME")
 
-        with tf.variable_scope('conv1_2'):
-            channels = 4; filters = 8; size = 3; stride = 1;
-            X_batch_conv1_2 = ops.conv3d(X_batch_conv1, channels, filters, stride, size, size, size, padding='SAME')
+kernel_size = 64
+stride = 1
+filter_depth = 2
+filter_height = 2
+filter_width = 2
+conv_layer_2 = ops.conv3d_block(pool_layer_1, training_flag, kernel_size, stride, filter_depth, filter_height,
+                                filter_width, regularizer, 2, padding="SAME")
 
-        with tf.variable_scope('pool1'):
-            size = 2; stride = 2;
-            X_batch_pool1 = tf.nn.max_pool3d(X_batch_conv1_2, ksize=[1, size, size, size, 1],
-                                             strides=[1, stride, stride, stride, 1], padding="SAME")
+pool_size = 2
+pool_stride = pool_size
+pool_layer_2 = tf.nn.max_pool3d(conv_layer_2, [1, pool_size, pool_size, pool_size, 1],
+                                [1, pool_stride, pool_stride, pool_stride, 1], padding="SAME")
 
-        with tf.variable_scope('bn2'):
-            X_batch_norm2 = tf.layers.batch_normalization(X_batch_pool1, training=training_flag)
+# kernel_size = 128
+# stride = 1
+# filter_depth = 5
+# filter_height = 5
+# filter_width = 5
+# conv_layer_3 = ops.conv3d_block(pool_layer_2, training_flag, kernel_size, stride, filter_depth, filter_height,
+#                                 filter_width, regularizer, 3, padding="SAME")
+#
+# pool_size = 2
+# pool_stride = pool_size
+# pool_layer_3 = tf.nn.max_pool3d(conv_layer_2, [1, pool_size, pool_size, pool_size, 1],
+#                                 [1, pool_stride, pool_stride, pool_stride, 1], padding="SAME")
 
-        with tf.variable_scope('conv2'):
-            channels = 8; filters = 16; size = 3; stride = 1;
-            X_batch_conv2 = ops.conv3d(X_batch_norm2, channels, filters, stride, size, size, size, padding='SAME')
+dense_1 = ops.dense_block(pool_layer_2, 2048, regularizer, 1)
 
-        with tf.variable_scope('conv2_2'):
-            channels = 16; filters = 32; size = 3; stride = 1;
-            X_batch_conv2_2 = ops.conv3d(X_batch_conv2, channels, filters, stride, size, size, size, padding='SAME')
+dense_1 = tf.nn.relu(dense_1)
 
-        with tf.variable_scope('pool2'):
-            size = 2; stride = 2;
-            X_batch_pool2 = tf.nn.max_pool3d(X_batch_conv2_2, ksize=[1, size, size, size, 1],
-                                             strides=[1, stride, stride, stride, 1], padding="SAME")
+dense_1 = tf.nn.dropout(dense_1, keep_prob)
 
-        conv_out_shape = X_batch_pool2.get_shape().as_list()
-        conv_out_shape[0] = 1
-        dim = reduce(lambda x, y: x * y, conv_out_shape)
+dense_2 = tf.layers.dense(dense_1, 2048)
 
-        X_batch_reshape = tf.reshape(X_batch_pool2, [-1, dim])
+dense_2 = tf.nn.relu(dense_2)
 
-        with tf.variable_scope('linear1'):
-            out_dim = 512
-            X_batch_linear1 = ops.linear_layer(X_batch_reshape, dim, out_dim)
+dense_2 = tf.nn.dropout(dense_2, keep_prob)
 
-        with tf.variable_scope('linear2'):
-            out_dim = label_size; dim = 512;
-            X_batch_linear2 = ops.linear_layer(X_batch_linear1, dim, out_dim)
+dense_3 = tf.layers.dense(dense_2, 2048)
 
-        logits = X_batch_linear2
+dense_3 = tf.nn.relu(dense_3)
 
-        return logits
+dense_3 = tf.nn.dropout(dense_3, keep_prob)
 
+dense_4 = tf.layers.dense(dense_3, 19)
 
-with tf.device(device):
-    with tf.variable_scope('network') as current_scope:
-        logits = build_network(X_batch, training_flag=True)
-        cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=Y_batch, logits=logits)
-        loss = tf.reduce_mean(cross_entropy)
-        grad_var_list = optimizer.compute_gradients(loss)
-        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-        with tf.control_dependencies(update_ops):
-            apply_gradients_op = optimizer.apply_gradients(grad_var_list, global_step)  # this is training op
+logit = dense_4
 
-        current_scope.reuse_variables()
-        logits2 = build_network(X_batch, training_flag=False)
-        Y_prediction = tf.nn.sigmoid(logits2)  # this is evaluation op
+# Prediction Loss
 
-epochs = 30
-batch_size = 300
+Y_prediction = tf.round(tf.nn.sigmoid(logit))
+
+# Training Loss
+
+cross_entropy = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=Y_batch, logits=logit))
+
+loss = cross_entropy
+
+# Add Regularization
+
+if regularizer_scale > 0:
+    reg_variables = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    reg_term = tf.contrib.layers.apply_regularization(regularizer, reg_variables)
+else:
+    reg_term = 0
+
+loss = loss + reg_term
+
+# Gradient
+
+params = tf.trainable_variables()
+gradients = tf.gradients(loss, params)
+
+global_step = tf.Variable(0, trainable=False)
+learning_rate = tf.train.exponential_decay(starting_learning_rate, global_step,
+                                           decay_step, decay_rate, staircase=True)
+
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+update_step = optimizer.apply_gradients(zip(gradients, params), global_step=global_step)
+
+# Model Persistence
+
+saver = tf.train.Saver()
+
+# Training
 
 sess.run(tf.global_variables_initializer())
 
-for _ in range(epochs):
-    train_X, train_binary_Y = shuffle(train_X, train_binary_Y)
-    i = 0
-    while i < len(train_X):
-        i_end = min(i + batch_size, len(train_X))
-        report_loss, _ = sess.run([loss, apply_gradients_op], feed_dict={
-            X_batch: np.expand_dims(train_X[i:i_end], axis=-1), Y_batch: train_binary_Y[i:i_end]})
-        print report_loss
-        i = i_end
+epochs = 100
+batch_size = 64
+report_step = 1000
+saved_mdl_name = 'result.mdl'
+
+best_subset_accuracy = 0
+
+if not infer_only:
+
+    for epoch in range(epochs):
+
+        train_X, train_Y = shuffle(train_X, train_Y)
+
+        i = 0
+        i_report = report_step
+        while i < len(train_X):
+            i_end = min(i + batch_size, len(train_X))
+            report_Y_prediction, _ = sess.run([Y_prediction, update_step],
+                                              feed_dict={
+                                                  X_batch: train_X[i:i_end],
+                                                  Y_batch: train_Y[i:i_end],
+                                                  keep_prob: 0.5,
+                                                  training_flag: True})
+            if i > i_report:
+                auc = roc_auc_score(train_Y[i:i_end], report_Y_prediction, average='micro')
+                subset_accuracy = accuracy_score(train_Y[i:i_end], report_Y_prediction)
+                print(str.format("Epoch %d train auc is %f and subset accuracy is %f"
+                                 % (epoch, auc, subset_accuracy)))
+                i_report += report_step
+            i = i_end
+
+        i = 0
+        auc_list = []
+        subset_accuracy_list = []
+        while i < len(test_X):
+            i_end = min(i + batch_size, len(test_X))
+            report_Y_prediction = sess.run(Y_prediction,
+                                           feed_dict={
+                                               X_batch: test_X[i:i_end],
+                                               keep_prob: 1,
+                                               training_flag: False})
+            auc_list.append(roc_auc_score(test_Y[i:i_end], report_Y_prediction, average='micro'))
+            subset_accuracy_list.append(accuracy_score(test_Y[i:i_end], report_Y_prediction))
+            i = i_end
+        print(str.format("Epoch %d test  auc is %f and subset accuracy is %f"
+                         % (epoch, float(np.mean(auc_list)), float(np.mean(subset_accuracy_list)))))
+        if np.mean(subset_accuracy_list) >= best_subset_accuracy:
+            saver.save(sess, saved_mdl_name)
+            best_subset_accuracy = np.mean(subset_accuracy_list)
+            print("Updated best model.")
+        print("**************************************************************")
+
+# Generate Validation Data
+
+print("Generating Validation Submission...")
+
+valid_test_X = np.load('../data/valid_test_X.npy')
+valid_test_X = utils.normalized_data(valid_test_X)
+valid_test_Y = np.zeros([len(valid_test_X), 19])
+
+if infer_only:
+    saver.restore(sess, saved_mdl_name)
+
+i = 0
+while i < len(valid_test_X):
+    i_end = min(i + batch_size, len(valid_test_X))
+    valid_Y_prediction = sess.run(Y_prediction,
+                                  feed_dict={
+                                      X_batch: valid_test_X[i:i_end, :, :, :, np.newaxis],
+                                      keep_prob: 1,
+                                      training_flag: False})
+    valid_test_Y[i:i_end] = valid_Y_prediction
+    i = i_end
+
+np.save('result', valid_test_Y)
+
+print("Done.")
--- model/ops.py
+++ model/ops.py
@@ -1,24 +1,50 @@
 import tensorflow as tf
-import tensorflow.contrib as tfc
 
 
-def conv3d(input_, channels, filters, stride, filter_d, filter_h, filter_w, padding="SAME"):
-    w = tf.get_variable('w', [filter_d, filter_h, filter_w, channels, filters],
-                        dtype=tf.float32,
-                        initializer=tfc.layers.variance_scaling_initializer(1.0, mode='FAN_AVG',
-                                                                            uniform=True))
-    b = tf.get_variable('b', [filters], dtype=tf.float32, initializer=tf.constant_initializer(0.1))
-    conv = tf.nn.conv3d(input_, w, [1, stride, stride, stride, 1], padding=padding)
-    pre_acitivation = tf.nn.bias_add(conv, b)
-    conv_out = tf.nn.relu(pre_acitivation)
-    return conv_out
+def conv3d(input_, output_channels, stride, filter_depth, filter_height, filter_width, regularizer, layer_no,
+           padding="SAME"):
+    with tf.variable_scope('conv3d_layer_' + str(layer_no), regularizer=regularizer):
+        w = tf.get_variable('w', [filter_depth, filter_height, filter_width, input_.shape[-1], output_channels],
+                            dtype=tf.float32,
+                            initializer=tf.contrib.layers.variance_scaling_initializer(1.0, mode='FAN_AVG',
+                                                                                       uniform=True))
+        b = tf.get_variable('b', [output_channels], dtype=tf.float32, initializer=tf.constant_initializer(0.0))
+
+        conv = tf.nn.conv3d(input_, w, [1, stride, stride, stride, 1], padding=padding)
+
+        return tf.nn.bias_add(conv, b)
 
 
-def linear_layer(linear_in, dim, hiddens):
-    weights = tf.get_variable('weights', [dim, hiddens], tf.float32,
-                              initializer=tfc.layers.variance_scaling_initializer(mode='FAN_AVG', uniform=True))
-    bias = tf.get_variable('bias', [hiddens], tf.float32,
-                           initializer=tf.constant_initializer(0.1))
-    pre_activations = tf.add(tf.matmul(linear_in, weights), bias)
-    linear_out = tf.nn.relu(pre_activations)
-    return linear_out
+def conv2d(input_, output_channels, stride, filter_depth, filter_height, layer_no, padding="SAME"):
+    with tf.variable_scope('conv2d_layer_' + str(layer_no)):
+        w = tf.get_variable('w', [filter_depth, filter_height, input_.shape[-1], output_channels],
+                            dtype=tf.float32,
+                            initializer=tf.truncated_normal_initializer(stddev=0.02))
+        b = tf.get_variable('b', [output_channels], dtype=tf.float32, initializer=tf.constant_initializer(0.0))
+
+        conv = tf.nn.conv2d(input_, w, [1, stride, stride, 1], padding=padding)
+
+        return tf.nn.bias_add(conv, b)
+
+
+def conv3d_block(X_, training_flag, kernel_size, conv_stride, filter_depth, filter_height, filter_width, regularizer,
+                 layer_no, padding="SAME"):
+    with tf.variable_scope('conv_block_' + str(layer_no)):
+        conv1 = conv3d(X_, kernel_size, conv_stride, filter_depth, filter_height, filter_width, regularizer, 1, padding=padding)
+        alpha = 0
+        relu1 = tf.maximum(conv1, alpha * conv1)
+    return relu1
+
+
+def dense_block(X_, output_channel, regularizer, layer_no):
+    with tf.variable_scope('dense_block_' + str(layer_no), regularizer=regularizer):
+        X_dim = X_.get_shape().as_list()
+        X_dim[0] = 1
+        X_dim = reduce(lambda x, y: x * y, X_dim)
+        reshape = tf.reshape(X_, [-1, X_dim])
+        return tf.layers.dense(reshape, output_channel)
+
+
+def gaussian_noise_layer(input_layer, std, training_flag):
+    noise = tf.random_normal(shape=tf.shape(input_layer), mean=0.0, stddev=std, dtype=tf.float32)
+    return tf.cond(training_flag, lambda: input_layer + noise, lambda :input_layer)
